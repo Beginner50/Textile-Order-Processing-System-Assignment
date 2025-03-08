10c546233a33773eec227814164a32dae889ce9b
Prashant Jatoo <jatooprashant099@gmail.com>
2025-03-08 15:34:09 +0400

    JDBC Setup Correction

--- src/Application.java
+++ src/Application.java
@@ -5,16 +5,18 @@ public class Application{
     public static void main(String[] args) {
         //     url -> "jdbc:mysql://hostname:dbms_port/database"
         String url = "jdbc:mysql://localhost:3306/order_processing_system";
-        Connection conn = null;
+        Connection conn;
         try{
             conn = DriverManager.getConnection(url);
-            conn.createStatement().execute("show tables;");
+//            conn.createStatement().execute("show tables;");
+
+            ToolTipManager.sharedInstance().setInitialDelay(200); // Set tooltip delay
+            LoginScreen loginScreen = new LoginScreen(conn);
+            loginScreen.setVisible(true);
+
             conn.close();
         } catch (Exception e){
             System.out.println("Error: " + e);
         }
-        ToolTipManager.sharedInstance().setInitialDelay(200); // Set tooltip delay
-        LoginScreen loginScreen = new LoginScreen(conn);
-        loginScreen.setVisible(true);
     }
 }
