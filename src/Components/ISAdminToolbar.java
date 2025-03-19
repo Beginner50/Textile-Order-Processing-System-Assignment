fb405d0ef413a5f94800a83902674f65ce104d03
Prashant Jatoo <jatooprashant099@gmail.com>
2025-03-19 12:40:06 +0400

    Removed redundant buttons

--- src/Components/ISAdminToolbar.java
+++ src/Components/ISAdminToolbar.java
@@ -16,9 +16,9 @@ public class ISAdminToolbar extends AbstractToolbar{
         this.addCrudButton("UpdateQuotation", ButtonFactory.createButton("Edit Quotation", "edit.png"), crudButtonHandler);
         this.addCrudButton("DeleteQuotation", ButtonFactory.createButton("Delete Quotation", "delete.png"), crudButtonHandler);
         this.addCrudButton("ConvertQuotation", ButtonFactory.createButton("Convert to Order", "convert.png"), crudButtonHandler);
-        this.addCrudButton("CreateOrder", ButtonFactory.createButton("Add Order", "add.png"), crudButtonHandler);
+//        this.addCrudButton("CreateOrder", ButtonFactory.createButton("Add Order", "add.png"), crudButtonHandler);
         this.addCrudButton("UpdateOrder", ButtonFactory.createButton("Edit Order", "edit.png"), crudButtonHandler);
-        this.addCrudButton("DeleteOrder", ButtonFactory.createButton("Delete Order", "delete.png"), crudButtonHandler);
+//        this.addCrudButton("DeleteOrder", ButtonFactory.createButton("Delete Order", "delete.png"), crudButtonHandler);
     }
 
     @Override
@@ -37,9 +37,9 @@ public class ISAdminToolbar extends AbstractToolbar{
                     currentTable.addRow(orderFromQuotation);
                     orderFromQuotation = null;
                 }
-                crudButtons.get("CreateOrder").setVisible(true);
+//                crudButtons.get("CreateOrder").setVisible(true);
                 crudButtons.get("UpdateOrder").setVisible(true);
-                crudButtons.get("DeleteOrder").setVisible(true);
+//                crudButtons.get("DeleteOrder").setVisible(true);
                 break;
             case "Bill":
                 break;
@@ -54,7 +54,8 @@ public class ISAdminToolbar extends AbstractToolbar{
             Object source = actionEvent.getSource();
             if (source == crudButtons.get("CreateQuotation")) {
                 new QuotationForm(null, currentTable).setVisible(true);
-            } else if (source == crudButtons.get("UpdateQuotation")) {
+            }
+            else if (source == crudButtons.get("UpdateQuotation")) {
                 int selectedRow = currentTable.getSelectedRow();
                 if (selectedRow != -1) {
                     // Get data from selected row
@@ -67,7 +68,8 @@ public class ISAdminToolbar extends AbstractToolbar{
                     JOptionPane.showMessageDialog(null, "Please select a quotation to edit",
                             "No Selection", JOptionPane.WARNING_MESSAGE);
                 }
-            } else if (source == crudButtons.get("DeleteQuotation")) {
+            }
+            else if (source == crudButtons.get("DeleteQuotation")) {
                 int selectedRow = currentTable.getSelectedRow();
                 if (selectedRow != -1) {
                     int confirm = JOptionPane.showConfirmDialog(null,
@@ -81,7 +83,8 @@ public class ISAdminToolbar extends AbstractToolbar{
                     JOptionPane.showMessageDialog(null, "Please select a quotation to delete",
                             "No Selection", JOptionPane.WARNING_MESSAGE);
                 }
-            } else if (source == crudButtons.get("ConvertQuotation")) {
+            }
+            else if (source == crudButtons.get("ConvertQuotation")) {
                 int selectedRow = currentTable.getSelectedRow();
                 if (selectedRow != -1) {
                     int confirm = JOptionPane.showConfirmDialog(null,
@@ -115,9 +118,11 @@ public class ISAdminToolbar extends AbstractToolbar{
                     JOptionPane.showMessageDialog(null, "Please select a quotation to convert",
                             "No Selection", JOptionPane.WARNING_MESSAGE);
                 }
-            } else if (source == crudButtons.get("CreateOrder")) {
-                new OrderForm(null, currentTable).setVisible(true);
-            } else if (source == crudButtons.get("UpdateOrder")) {
+
+//            } else if (source == crudButtons.get("CreateOrder")) {
+//                new OrderForm(null, currentTable).setVisible(true);
+            }
+            else if (source == crudButtons.get("UpdateOrder")) {
                 int selectedRow = currentTable.getSelectedRow();
                 if (selectedRow != -1) {
                     // Get data from selected row
@@ -130,21 +135,22 @@ public class ISAdminToolbar extends AbstractToolbar{
                     JOptionPane.showMessageDialog(null, "Please select an order to edit",
                             "No Selection", JOptionPane.WARNING_MESSAGE);
                 }
-            } else if (source == crudButtons.get("DeleteOrder")) {
-                int selectedRow = currentTable.getSelectedRow();
-                if (selectedRow != -1) {
-                    int confirm = JOptionPane.showConfirmDialog(null,
-                            "Are you sure you want to delete this order?",
-                            "Confirm Deletion", JOptionPane.YES_NO_OPTION);
-                    if (confirm == JOptionPane.YES_OPTION) {
-                        ((DefaultTableModel) currentTable.getModel()).removeRow(
-                                currentTable.convertRowIndexToModel(selectedRow));
-                    }
-                } else {
+            }
+//            } else if (source == crudButtons.get("DeleteOrder")) {
+//                int selectedRow = currentTable.getSelectedRow();
+//                if (selectedRow != -1) {
+//                    int confirm = JOptionPane.showConfirmDialog(null,
+//                            "Are you sure you want to delete this order?",
+//                            "Confirm Deletion", JOptionPane.YES_NO_OPTION);
+//                    if (confirm == JOptionPane.YES_OPTION) {
+//                        ((DefaultTableModel) currentTable.getModel()).removeRow(
+//                                currentTable.convertRowIndexToModel(selectedRow));
+//                    }
+                else {
                     JOptionPane.showMessageDialog(null, "Please select an order to delete",
                             "No Selection", JOptionPane.WARNING_MESSAGE);
                 }
             }
         }
-    }
 }
+
