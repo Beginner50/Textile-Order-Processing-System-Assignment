187a3204e7fcdef36c4f2863b40fe4ab871d9eca
tir-p <aniketparbutee@gmail.com>
2025-03-21 12:41:36 +0400

    Added user  and pw to Conn

--- app/src/main/java/tops/Application.java
+++ app/src/main/java/tops/Application.java
@@ -3,14 +3,16 @@ package tops;
 import javax.swing.*;
 import java.sql.*;
 
-public class Application{
+public class Application {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        //    url -> "jdbc:mysql://hostname:dbms_port/database"
-        String url = "jdbc:mysql://localhost:3306/order_processing_system";
+        // url -> "jdbc:mysql://hostname:dbms_port/database"
+        String url = "jdbc:mysql://localhost:3306/textileorderprocessing";
+        String user = "root"; // Add your username here
+        String password = ""; // Add your password here
         Connection conn = null;
-        try{
-            conn = DriverManager.getConnection(url);
+        try {
+            conn = DriverManager.getConnection(url, user, password);
             conn.createStatement().execute("show tables;");
 
             ToolTipManager.sharedInstance().setInitialDelay(200); // Set tooltip delay
@@ -18,7 +20,7 @@ public class Application{
             loginScreen.setVisible(true);
 
             conn.close();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
